18f0e2913fbdb2b045cd761433a9de01e719e6ec
kolawole kolapo <pappy.kay9@gmail.com>
2025-03-14 19:33:42 +0100

    about page change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import ClientsSection from '@/components/clients-section'
 import AwardsSection from '@/components/awards-section'
 
 export const metadata = {
-  title: 'About • Anthony Wiktor Award-Winning Creative Director and Designer Who Moves Brands Forward',
-  description: 'Anthony Wiktor is a Webby Award-Winning Creative Director and Designer based in Los Angeles specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
+  title: 'About • Elizabeth Jikemi Award-Winning Project Manager and NGO Specialist',
+  description: 'Elizabeth Jikemi is a Super Award-Winning Project Manager and NGO Specialist based in Indiana specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
 }
 
 export default function About() {
