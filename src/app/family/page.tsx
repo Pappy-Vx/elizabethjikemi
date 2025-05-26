87b3ded784d42f045d3d7c9e7f070b56b200cca2
kolawole kolapo <pappy.kay9@gmail.com>
2025-05-26 13:24:30 +0000

    home category menu updated

---
+++ src/app/family/page.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next"
+import ExpertiseSection from '@/components/expertise-section';
+import ClientsSection from '@/components/clients-section';
+import AwardsSection from '@/components/awards-section';
+import Header from "@/components/header";
+import AboutHero from "@/components/about-hero";
+import Image from "next/image";
+
+export const metadata = {
+     title: 'Family • Elizabeth Jikiemi Award-Winning Project Manager and NGO Specialist',
+     description: 'Elizabeth Jikiemi is a Super Award-Winning Project Manager and NGO Specialist based in Indiana specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
+   }
+
+export default function Family() {
+  return (
+<>
+      {/* Fixed Background Image */}
+      <div className="fixed top-0 left-0 w-full h-full z-0">
+        <Image
+          src="/family.jpg"
+          alt="Elizabeth Jikiemi"
+          fill
+          style={{ objectFit: "cover", pointerEvents: "none" }}
+          priority
+        />
+      </div> 
+
+      {/* Foreground Content */}
+      <Header />
+      <AboutHero />
+      {/* <BioSection /> */}
+      <ExpertiseSection />
+      {/*   */}
+      <ClientsSection />
+      <AwardsSection />
+</>
+
+
+    
+  );
+}
