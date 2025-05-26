8bbaa4b82391da49ea8f05b7143623d5b8d41b34
kolawole kolapo <pappy.kay9@gmail.com>
2025-05-26 14:21:27 +0000

    pages link completed

--- src/app/nigeria/page.tsx
+++ src/app/nigeria/page.tsx
@@ -7,18 +7,18 @@ import AboutHero from "@/components/about-hero";
 import Image from "next/image";
 
 export const metadata = {
-     title: 'Family • Elizabeth Jikiemi Award-Winning Project Manager and NGO Specialist',
+     title: 'Nigeria • Elizabeth Jikiemi Award-Winning Project Manager and NGO Specialist',
      description: 'Elizabeth Jikiemi is a Super Award-Winning Project Manager and NGO Specialist based in Indiana specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
    }
 
-export default function Family() {
+export default function Nigeria() {
   return (
 <>
       {/* Fixed Background Image */}
       <div className="fixed top-0 left-0 w-full h-full z-0">
         <Image
-          src="/family.jpg"
-          alt="family"
+          src="/nigeria.jpg"
+          alt="nigeria"
           fill
           style={{ objectFit: "cover", pointerEvents: "none" }}
           priority
