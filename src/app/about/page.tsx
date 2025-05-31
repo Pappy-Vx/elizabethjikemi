63a684f1861e9d221f401593b3bd0b4e094bf417
kolawole kolapo <pappy.kay9@gmail.com>
2025-05-31 14:07:55 +0000

    about page background rx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,23 +8,37 @@ import AboutHero from "@/components/about-hero";
 import Image from "next/image";
 
 export const metadata = {
-     title: 'About • Elizabeth Jikiemi Award-Winning Project Manager and NGO Specialist',
-     description: 'Elizabeth Jikiemi is a Super Award-Winning Project Manager and NGO Specialist based in Indiana specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
-   }
+  title: 'About • Elizabeth Jikiemi Award-Winning Project Manager and NGO Specialist',
+  description: 'Elizabeth Jikiemi is a Super Award-Winning Project Manager and NGO Specialist based in Indiana specializing in branding, design, interactive, and advertising to help challenger brands break through the clutter and outsmart the competition.',
+}
 
 export default function About() {
   return (
-<>
+    <>
       {/* Fixed Background Image */}
-      <div className="fixed top-0 left-0 w-full h-full z-0">
-        <Image
-          src="/me.jpg"
-          alt="Elizabeth Jikiemi"
-          fill
-          style={{ objectFit: "cover", pointerEvents: "none" }}
-          priority
-        />
-      </div> 
+      <div className="sm:invisible visible">
+        <div className="fixed bottom-[-12rem] left-[-14rem] w-[40rem] h-full z-0">
+          <Image
+            src="/me.jpg"
+            alt="Elizabeth Jikiemi"
+            fill
+            style={{ objectFit: "contain", pointerEvents: "none" }}
+            priority
+          />
+        </div>
+      </div>
+      <div className="sm:visible invisible">
+        <div className="fixed top-0 left-0 w-full h-full z-0">
+          <Image
+            src="/me.jpg"
+            alt="Elizabeth Jikiemi"
+            fill
+            style={{ objectFit: "cover", pointerEvents: "none" }}
+            priority
+          />
+        </div>
+      </div>
+
 
       {/* Foreground Content */}
       <Header />
@@ -34,9 +48,9 @@ export default function About() {
       {/*   */}
       <ClientsSection />
       <AwardsSection />
-</>
+    </>
 
 
-    
+
   );
 }
